4dc88e871074968c6bc7419c844fc5b01b44894a
sameerpadghan <96347400+sameerpadghan@users.noreply.github.com>
2021-12-18 21:50:27 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,7 +4,7 @@ import Encrypt
 import Decrypt
 if __name__=="__main__":
     g=GenerateKey.GenerateKey("Password")
-    l=LoadKey.LoadKey("C:\\Users\\SAMEE\\Documents\\Programming\\PythonMzos\\Password.txt")
+    l=LoadKey.LoadKey("C:\\Users\\SAMEE\\Documents\\Programming\\Password.txt")
     print(l.key())
-    Encrypt.Encrypt("C:\\Users\\SAMEE\\Documents\\Programming\\PythonMzos\\Sameer.txt",l.key())
-    Decrypt.Decrypt("C:\\Users\\SAMEE\\Documents\\Programming\\PythonMzos\\Sameer.txt",l.key())
+    Encrypt.Encrypt("C:\\Users\\SAMEE\\Documents\\Programming\\Sam.txt",l.key())
+    Decrypt.Decrypt("C:\\Users\\SAMEE\\Documents\\Programming\\Same.txt",l.key())
